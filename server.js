5e92f0146e3c072144e61624e230e4e9953458dd
rcolet <remi.colet@icloud.com>
2019-09-24 04:43:11 +0200

    Finish Back-end

--- server.js
+++ server.js
@@ -1,15 +1,41 @@
-// server.js
-// load the things we need
+var fs = require('fs');
+var cors = require('cors');
 var express = require('express');
 var app = express();
+app.use(cors());
 
-// set the view engine to ejs
-app.set('view engine', 'ejs');
-
-// index page 
+//Mise en place de la route appelé par le coté client
 app.get('/', function(req, res) {
-    res.render('pages/sirens_fxt');
+    res.json(dedupe('sirens_fxt.txt'));
 });
 
-app.listen(8080);
-console.log('8080 is the magic port');
+//Fonction de lecture du fichier
+let read_file = (file) => {
+    return fs.readFileSync('sirens_fxt.txt', 'utf8').split('\n');
+}
+
+//Fonction de dédoublonnage et de recherche des statistique rechercher 
+let dedupe = (file) => {
+    sirens = read_file(file);
+    unique = 0;//Compteur de SIRENs unique
+    multiple = 0;//Compteur de SIRENs non unique
+    let sirens_tab = {}; //table de dédoublonnage avec nombre de répétition
+    sirens.forEach(element => {
+        if(sirens_tab[element] != undefined){//Si doublon
+            sirens_tab[element]++;
+            unique--;//Enlève 1 car le SIRENs selectionné n'est plus unique
+            multiple++;
+        } else {//Si le SIRENs viens d'être trouvé pour la première fois
+            sirens_tab[element] = 1;
+            unique++;
+        }
+    });
+    return {
+        sirens_tab,
+        unique,
+        multiple
+    }
+}
+
+app.listen(7777);
+console.log('7777 is the magic port');
